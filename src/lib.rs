4a5845dfd5e4d8b4c6fef55b71987635ae4fd55e
Manuel Schölling <manuel.schoelling@gmx.de>
2019-01-04 20:46:10 +0100

    Move to tokio

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,58 @@
+#![feature(pin)]
+#![feature(await_macro, async_await, futures_api)]
 #![allow(dead_code)]
 
-#[macro_use] extern crate log;
+#[macro_use] extern crate bitflags;
+#[macro_use] extern crate tokio;
 extern crate libc;
 
-use tuntap::{TunTap,IFF_TUN,IFF_TAP,IFF_NO_PI};
-use std::thread::Thread;
+//use tuntap::{TunTap,IFF_TUN,IFF_TAP,IFF_NO_PI};
 
 mod tuntap;
+pub use crate::tuntap::*;
 
 #[test]
 fn it_works() {
-	let flags = IFF_TUN | IFF_NO_PI;
-	let (tun1, (tx1,rx1)) = TunTap::new(flags).unwrap();
-	let (tun2, (tx2,rx2)) = TunTap::new(flags).unwrap();
+	use tokio::prelude::*;
 
-	for (i, tun) in [tun1, tun2].iter().enumerate() {
-		tun.set_owner(1001).unwrap();
-		tun.set_group(1001).unwrap();
+	tokio::run_async(async {
+		let flags = crate::TunTapFlags::IFF_TUN | crate::TunTapFlags::IFF_NO_PI;
+		let (mut tun1, (mut tx1, mut rx1)) = await!(TunTap::new(flags)).unwrap();
+		let (mut tun2, (mut tx2, mut rx2)) = await!(TunTap::new(flags)).unwrap();
+
+		tun1.set_owner(1001).unwrap();
+		tun1.set_group(1001).unwrap();
 		//tun.set_mac([11,22,33,44,55,66+i as u8]).unwrap();
-		if i == 0 {
-			tun.set_ipv4("10.0.0.1").unwrap();
-			tun.set_ipv6("fe80::0db8:1234:1211").unwrap();
-		} else {
-			tun.set_ipv4("10.0.0.2").unwrap();
-			tun.set_ipv6("fe80::0db8:1234:1212").unwrap();
-		}
-		tun.set_mtu(1400).unwrap();
-		tun.set_up().unwrap();
-	}
 
-	Thread::spawn(move || {
-		for packet in rx1.iter() {
-			tx2.send(packet).unwrap();
+		tun1.set_ip("10.0.0.1".parse().unwrap()).unwrap();
+		tun1.set_ip("fe80::0db8:1234:1211".parse().unwrap()).unwrap();
+
+		tun1.set_mtu(1400).unwrap();
+		tun1.set_up().unwrap();
+
+
+		tun2.set_owner(1001).unwrap();
+		tun2.set_group(1001).unwrap();
+		//tun.set_mac([11,22,33,44,55,66+i as u8]).unwrap();
+
+		tun2.set_ip("10.0.0.2".parse().unwrap()).unwrap();
+		tun2.set_ip("fe80::0db8:1234:1212".parse().unwrap()).unwrap();
+
+		tun2.set_mtu(1400).unwrap();
+		tun2.set_up().unwrap();
+
+		tokio::spawn_async(async move {
+			println!("send0");
+			while let Some(Ok(packet)) = await!(rx1.next()) {
+				println!("rx1 {}", packet.len());
+				await!(tx2.send_async(packet)).unwrap();
+			}
+		});
+
+		while let Some(Ok(packet)) = await!(rx2.next()) {
+			println!("rx2 {}", packet.len());
+			await!(tx1.send_async(packet)).unwrap();
 		}
-	}).detach();
-	for packet in rx2.iter() {
-		tx1.send(packet).unwrap();
-	}
+	})
 }
 	
--- src/tuntap.rs
+++ src/tuntap.rs
@@ -1,18 +1,22 @@
-use std::io::{File, Open, ReadWrite};
+use std::path::Path;
 use std::default::Default;
-use std::os::unix::prelude::{AsRawFd, Fd};
-use libc::funcs::posix88::unistd::write;
-use libc::funcs::bsd43::socket;
-use libc::consts::os::bsd44::{AF_INET,AF_INET6,SOCK_DGRAM};
-use libc::types::common::c95::{c_void};
-use libc::types::os::arch::c95::{c_char,c_ulong,c_ushort,c_int};
-use std::io::{IoResult,IoError};
-use libc::types::os::common::bsd44::{sockaddr_in,sockaddr,in_addr,in6_addr};
+use std::fs::OpenOptions;
+use std::os::unix::prelude::AsRawFd;
+use std::os::unix::io::RawFd;
+use std::io::Result as IoResult;
+use std::io::Error as IoError;
 
-use std::sync::mpsc::{channel,Sender,Receiver};
-use std::thread::Thread;
-
-const IFNAMSIZ: uint = 16;
+use libc::write;
+use libc::socket;
+use libc::{AF_INET, AF_INET6, SOCK_DGRAM, IFNAMSIZ};
+use libc::c_void;
+use libc::{c_char,c_ushort,c_int};
+use libc::{in_addr,in6_addr};
+use nix::sys::socket::{sockaddr_in,sockaddr};
+use futures::sync::mpsc::Receiver;
+use futures::sync::mpsc::Sender;
+use tokio::prelude::*;
+use tokio::fs::File;
 
 #[repr(C)]
 struct InterfaceRequest16 {
@@ -95,44 +99,43 @@ type Uid = u32;
 type Gid = u32;
 
 extern "C" {
-	fn ioctl(fd: i32, icr: IoCtlRequest, some: c_ulong) -> i32;
-	fn inet_pton(af: c_int, src: *const c_char, dst: &mut ::libc::types::os::common::bsd44::in_addr) -> c_int;
+	//fn ioctl(fd: i32, icr: IoCtlRequest, some: c_ulong) -> i32;
+	fn inet_pton(af: c_int, src: *const c_char, dst: &mut libc::in_addr) -> c_int;
 }
 
 bitflags! {
-	flags TunTapFlags: u16 {
-		const IFF_UP        = 1<<0,
-		const IFF_RUNNING   = 1<<6,
-		const IFF_TUN       = 0x0001,
-		const IFF_TAP       = 0x0002,
-		const IFF_NO_PI     = 0x0100,
-		const IFF_ONE_QUEUE = 0x0200,
-		const IFF_VNET_HDR  = 0x0400,
-		const IFF_TUN_EXCL  = 0x0800,
+	pub struct TunTapFlags: u16 {
+		const IFF_UP        = 1<<0;
+		const IFF_RUNNING   = 1<<6;
+		const IFF_TUN       = 0x0001;
+		const IFF_TAP       = 0x0002;
+		const IFF_NO_PI     = 0x0100;
+		const IFF_ONE_QUEUE = 0x0200;
+		const IFF_VNET_HDR  = 0x0400;
+		const IFF_TUN_EXCL  = 0x0800;
 	}
 }
 
 bitflags! {
 	#[repr(C)]
-	flags IoCtlRequest: u32 {
-		const TUNSETIFF      = 0x400454ca,
-		const TUNSETOWNER    = 0x400454cc,
-		const TUNSETGROUP    = 0x400454ce,
+	struct IoCtlRequest: u64 {
+		const TUNSETIFF      = 0x400454ca;
+		const TUNSETOWNER    = 0x400454cc;
+		const TUNSETGROUP    = 0x400454ce;
 
-		const SIOCGIFFLAGS   = 0x8913,
-		const SIOCSIFFLAGS   = 0x8914,
-		const SIOCSIFADDR    = 0x8916,
-		const SIOCSIFMTU     = 0x8922,
-		const SIOCSIFNAME    = 0x8923,
-		const SIOCSIFHWADDR  = 0x8924,
-		const SIOCGIFINDEX   = 0x8933,
-		const SIOGIFINDEX    = 0x8933, // same as SIOCGIFINDEX
+		const SIOCGIFFLAGS   = 0x8913;
+		const SIOCSIFFLAGS   = 0x8914;
+		const SIOCSIFADDR    = 0x8916;
+		const SIOCSIFMTU     = 0x8922;
+		const SIOCSIFNAME    = 0x8923;
+		const SIOCSIFHWADDR  = 0x8924;
+		const SIOCGIFINDEX   = 0x8933;
+		const SIOGIFINDEX    = 0x8933; // same as SIOCGIFINDEX
 	}
 }
 
-#[derive(Copy)]
 pub struct TunTap {
-	fd:   Fd,
+	fd:    RawFd,
 	sock4: c_int,
 	sock6: c_int,
 	name: [u8; IFNAMSIZ],
@@ -140,11 +143,11 @@ pub struct TunTap {
 
 macro_rules! ioctl(
 	($fd:expr, $flags:expr, $value:expr) => ({
-		let ptr = ::std::mem::transmute($value);
-		let res = ioctl($fd, $flags, ptr);
+		//let ptr = ::std::mem::transmute($value);
+		let res = libc::ioctl($fd, $flags.bits(), $value);
 
 		if res < 0 {
-			Err(IoError::last_error())
+			Err(IoError::last_os_error())
 		} else {
 			Ok(())
 		}
@@ -152,38 +155,33 @@ macro_rules! ioctl(
 );
 
 impl TunTap {
-	pub fn new(flags: TunTapFlags)
-		-> IoResult<(TunTap,(Sender<Vec<u8>>,Receiver<Vec<u8>>))>
+	pub async fn new(flags: TunTapFlags)
+		-> IoResult<(TunTap,(Sender<Vec<u8>>, Receiver<Vec<u8>>))>
 	{
 		let p = Path::new("/dev/net/tun");
-		let mut file = match File::open_mode(&p, Open, ReadWrite) {
-			Ok(f) => f,
-			Err(e) => {
-				return Err(e);
-			}
-		};
+		let file = OpenOptions::new().write(true)
+							.read(true)
+							.open(p)?;
+		let fd = file.as_raw_fd();
+		let mut file = File::from_std(file);
 
 		let ifr_create = InterfaceRequest16 {
 			flags: flags.bits(),
 			..Default::default()
 		};
-		let create = unsafe { ioctl!(file.as_raw_fd(), TUNSETIFF, &ifr_create) };
-		if create.is_err() {
-			return Err(create.unwrap_err());
-		}
+		unsafe { ioctl!(fd, IoCtlRequest::TUNSETIFF, &ifr_create) }?;
 
 		const IPPROTO_IP: c_int = 0;
 		let sock4 = unsafe { socket(AF_INET, SOCK_DGRAM, IPPROTO_IP) };
 		if sock4 < 0 {
-			return Err(IoError::last_error())
+			return Err(IoError::last_os_error())
 		}
 
 		let sock6 = unsafe { socket(AF_INET6, SOCK_DGRAM, 0) };
 		if sock6 < 0 {
-			return Err(IoError::last_error())
+			return Err(IoError::last_os_error())
 		}
 
-		let fd = file.as_raw_fd();//unsafe { dup(self.file.as_raw_fd()) };
 		let tuntap = TunTap {
 			fd: fd,
 			sock4: sock4,
@@ -191,45 +189,45 @@ impl TunTap {
 			name: ifr_create.name,
 		};
 
-		let (your_tx,my_rx):(Sender<Vec<u8>>,_) = channel();
-		let (my_tx,your_rx) = channel();
+		let (your_tx, mut my_rx): (Sender<Vec<u8>>,_) = futures::sync::mpsc::channel(128 * 1024);
+		let (mut my_tx, your_rx): (Sender<Vec<u8>>,_) = futures::sync::mpsc::channel(128 * 1024);
 
-		Thread::spawn(move || {
-			for packet in my_rx.iter() {
+		tokio::spawn_async(async move {
+			while let Some(Ok(packet)) = await!(my_rx.next()) {
 				let ptr = packet.as_slice().as_ptr();
 				unsafe {
-					write(fd, ptr as *const c_void, packet.len() as u64);
+					write(fd, ptr as *const c_void, packet.len() as usize);
 				}
 			}
-		}).detach();
+		});
 
-		Thread::spawn(move || {
+		tokio::spawn_async(async move {
+			let mut buf = [0; 2048];
 			loop {
-				let mut packet = Vec::with_capacity(2024);
-				match file.push(2024, &mut packet) {
-					Ok(_) => my_tx.send(packet),
+				match await!(file.read_async(&mut buf)) {
+					Ok(n) => await!(my_tx.send_async(buf[..n].to_vec())).unwrap(),
 					_ => break
 				};
 			}
-		}).detach();
+		});
 
 		Ok((tuntap,(your_tx,your_rx)))
 	}
 
-	pub fn set_owner(self, owner: Uid) -> IoResult<()> {
-		unsafe { ioctl!(self.fd, TUNSETOWNER, owner as u64) }
+	pub fn set_owner(&mut self, owner: Uid) -> IoResult<()> {
+		unsafe { ioctl!(self.fd, IoCtlRequest::TUNSETOWNER, owner as u64) }
 	}
 
-	pub fn set_group(self, group: Gid) -> IoResult<()> {
-		unsafe { ioctl!(self.fd, TUNSETGROUP, group as u64) }
+	pub fn set_group(&mut self, group: Gid) -> IoResult<()> {
+		unsafe { ioctl!(self.fd, IoCtlRequest::TUNSETGROUP, group as u64) }
 	}
 
-	pub fn set_mtu(self, mtu: i32) -> IoResult<()> {
+	pub fn set_mtu(&mut self, mtu: i32) -> IoResult<()> {
 		let ifr = InterfaceRequest32 {
 			name: self.name,
 			flags: mtu,
 		};
-		unsafe { ioctl!(self.sock4, SIOCSIFMTU, &ifr) }
+		unsafe { ioctl!(self.sock4, IoCtlRequest::SIOCSIFMTU, &ifr) }
 	}
 
 	/*
@@ -246,21 +244,21 @@ impl TunTap {
 		}
 		unsafe { ioctl!(self.sock4, SIOCSIFHWADDR, &ifr) }
 	}*/
-
+/*
 	pub fn set_ipv4(self, ipv4: &'static str) -> IoResult<()> {
 		let mut ifr_ipaddr = InterfaceRequestSockaddrIn {
 			name:     self.name,
 			..Default::default()
 		};
 		ifr_ipaddr.sockaddr.sin_family = AF_INET as c_ushort;
-		let ip = ::std::ffi::CString::from_slice(ipv4.as_bytes());
+		let ip = ::std::ffi::CString::new(ipv4.as_bytes()).unwrap();
 		let res = unsafe { inet_pton(AF_INET, ip.as_ptr(),
 								&mut ifr_ipaddr.sockaddr.sin_addr) == 1 };
 		if !res {
-			return Err(IoError::last_error());
+			return Err(IoError::last_os_error());
 		}
 
-		unsafe { ioctl!(self.sock4, SIOCSIFADDR, &ifr_ipaddr) }
+		unsafe { ioctl!(self.sock4, IoCtlRequest::SIOCSIFADDR, &ifr_ipaddr) }
 	}
 
 	pub fn set_ipv6(self, ipv6: &'static str) -> IoResult<()> {
@@ -268,37 +266,69 @@ impl TunTap {
 			name: self.name,
 			..Default::default()
 		};
-		let res = unsafe { ioctl!(self.sock6, SIOGIFINDEX, &mut ifr) };
+		let res = unsafe { ioctl!(self.sock6, IoCtlRequest::SIOGIFINDEX, &mut ifr) };
 		if res.is_err() {
 			return Err(res.unwrap_err());
 		}
 
 		let mut ifr6 = InterfaceRequestIn6 {
-			addr:      in6_addr { s6_addr: [0;8] },
+			addr:      in6_addr { s6_addr: [0; 16] },
 			prefixlen: 64,
 			ifindex:   ifr.flags,
 		};
 
-		let ip = ::std::ffi::CString::from_slice(ipv6.as_bytes());
+		let ip = ::std::ffi::CString::new(ipv6.as_bytes()).unwrap();
 		let res = unsafe { inet_pton(AF_INET6, ip.as_ptr(),
 								::std::mem::transmute(&mut (ifr6.addr.s6_addr))) == 1 };
 		if !res {
-			return Err(IoError::last_error());
+			return Err(IoError::last_os_error());
 		}
-		unsafe { ioctl!(self.sock6, SIOCSIFADDR, &ifr6) }
+		unsafe { ioctl!(self.sock6, IoCtlRequest::SIOCSIFADDR, &ifr6) }
+	}*/
+
+	pub fn set_ip(&mut self, addr: std::net::IpAddr) -> IoResult<()> {
+		let addr = nix::sys::socket::InetAddr::from_std(&(addr, 0).into());
+		match addr {
+			nix::sys::socket::InetAddr::V4(sockaddr_in) => {
+				let mut ifr_ipaddr = InterfaceRequestSockaddrIn {
+					name:     self.name,
+					..Default::default()
+				};
+				ifr_ipaddr.sockaddr = sockaddr_in;
+
+				unsafe { ioctl!(self.sock4, IoCtlRequest::SIOCSIFADDR, &ifr_ipaddr) }?;
+				Ok(())
+			},
+			nix::sys::socket::InetAddr::V6(sockaddr_in6) => {
+				let mut ifr = InterfaceRequest32 {
+					name: self.name,
+					..Default::default()
+				};
+				unsafe { ioctl!(self.sock6, IoCtlRequest::SIOGIFINDEX, &mut ifr) }?;
+
+				let ifr6 = InterfaceRequestIn6 {
+					addr:      sockaddr_in6.sin6_addr,
+					prefixlen: 64,
+					ifindex:   ifr.flags,
+				};
+
+				unsafe { ioctl!(self.sock6, IoCtlRequest::SIOCSIFADDR, &ifr6) }?;
+				Ok(())
+			},
+		}
 	}
 
-	pub fn set_up(self) -> IoResult<()> {
+	pub fn set_up(&mut self) -> IoResult<()> {
 		let mut ifr_setup = InterfaceRequest16 {
 			name: self.name,
 			..Default::default()
 		};
-		let setup = unsafe { ioctl!(self.sock4, SIOCGIFFLAGS, &ifr_setup) };
+		let setup = unsafe { ioctl!(self.sock4, IoCtlRequest::SIOCGIFFLAGS, &ifr_setup) };
 		if setup.is_err() {
 			return Err(setup.unwrap_err());
 		}
 
-		ifr_setup.flags |= (IFF_UP | IFF_RUNNING).bits();
-		unsafe { ioctl!(self.sock4, SIOCSIFFLAGS, &ifr_setup) }
+		ifr_setup.flags |= (TunTapFlags::IFF_UP | TunTapFlags::IFF_RUNNING).bits();
+		unsafe { ioctl!(self.sock4, IoCtlRequest::SIOCSIFFLAGS, &ifr_setup) }
 	}
 }
